ecd73abb2874b9722f2d695c3c5e449bd1c636da
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2023-01-30 23:13:29 +0100

    add ToyForm, edit and delete for Toy

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,17 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Toy;
+use App\Form\ToyType;
+use App\Repository\BrandRepository;
+use App\Repository\ToyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(ToyRepository $toyRepository, BrandRepository $brandRepository): Response
     {
+        $toys = $toyRepository->findAll();
+        $brands = $brandRepository->findAll();
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'toys' => $toys,
+            'brands' => $brands,
         ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_toy_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Toy $toy, ToyRepository $toyRepository): Response
+    {
+        $toyEditForm = $this->createForm(ToyType::class, $toy);
+        $toyEditForm->handleRequest($request);
+
+        if ($toyEditForm->isSubmitted() && $toyEditForm->isValid()) {
+
+            $toyRepository->save($toy, true);
+
+            return $this->redirectToRoute('app_dashboard', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('dashboard/editToy.html.twig', [
+            'toy' => $toy,
+            'toyEditForm' => $toyEditForm,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_toy_delete', methods: ['POST'])]
+    public function delete(Request $request, Toy $toy, ToyRepository $toyRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $toy->getId(), $request->request->get('_token'))) {
+            $toyRepository->remove($toy, true);
+        }
+
+        return $this->redirectToRoute('app_dashboard', [], Response::HTTP_SEE_OTHER);
     }
 }
