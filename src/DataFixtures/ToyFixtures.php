d25e7c38574fb92f5d3a1152848f1b45778d3cdb
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2023-01-30 19:49:25 +0100

    add toy and brand fixtures

--- src/DataFixtures/ToyFixtures.php
+++ src/DataFixtures/ToyFixtures.php
@@ -2,20 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Toy;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
-class ToyFixtures extends Fixture
+class ToyFixtures extends Fixture implements DependentFixtureInterface
 {
-    const TOYS = [
-        ['title' => '', 'size' => "", 'numbercopies' => '', 'price' => '', 'linktopurchase' =>'', 'description' => '', 'picture' => ''],
-    ];
 
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for($j = 1; $j < 11; $j++) {
+            $toy = new Toy();
+            $toy->setTitle($faker->sentence(4));
+            $toy->setSize($faker->numberBetween(5, 30));
+            $toy->setNumbercopies($faker->numberBetween(200, 20000));
+            $toy->setDescription($faker->paragraphs(1, true));
+            $toy->setPrice($faker->numberBetween(30, 500));
+            $toy->setPicture('https://cdn.shopify.com/s/files/1/0088/7557/3306/products/22_3in_Graffiti_King9th_ProductIMG_R34_750x750.jpg?v=1666621811');
+            $toy->setLinktopurchase('https://superplastic.co/collections/janky-vinyl-designer-art-toys/products/king-janky-the-9th-graf-king');
+            $brand = $this->getReference('brand_'. $j);
+            $toy->setBrand($brand);
+            $manager->persist($toy);
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies() : array
+    {
+        return [
+            BrandFixtures::class,
+        ];
     }
 }
