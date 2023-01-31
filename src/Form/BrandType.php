18a4fcc04f0f7549266b30aa8668f4a382cd0dc7
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2023-01-31 21:56:13 +0100

    change some label fields in BrandType

--- src/Form/BrandType.php
+++ src/Form/BrandType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Brand;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
@@ -13,10 +16,13 @@ class BrandType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class)
             ->add('city')
             ->add('pictureFile', VichFileType::class, [
+                'label' => 'Picture',
                 'required'      => false,
                 'allow_delete'  => true,
                 'download_uri' => true,
